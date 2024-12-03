a13f4b91e61915a96d94eb0dd7d148af3a01281d
Derek Corniello <corniedj@mail.uc.edu>
2024-12-03 08:47:57 -0500

    added better site structure and donate button

--- vue/frontend/src/App.vue
+++ vue/frontend/src/App.vue
@@ -28,7 +28,16 @@
           </div>
         </div>
       </div>
-    </main>
+        <p>Please consider donating to allow sites like these to keep running!</p>    
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="business" value="R4KZAGAML2VMS" />
+            <input type="hidden" name="no_recurring" value="0" />
+            <input type="hidden" name="item_name" value="Thank you for donating! This will help me create new projects, and keep current ones running!" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+        </main>
   </div>
 </template>
 
